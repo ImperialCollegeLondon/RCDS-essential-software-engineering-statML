fd8916b5ef1fcf18766ed16d8bfc69bfaecb9b7a
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-03-03 16:08:08 +0000

    feat: add bubble sort

--- sort.py
+++ sort.py
@@ -11,3 +11,11 @@ def pivot_sort(arr):
 
     # Recursively sort the sub-arrays and combine them with the pivot
     return pivot_sort(left) + [pivot] + pivot_sort(right)
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
